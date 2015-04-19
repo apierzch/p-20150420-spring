0530364c17a86df3c43257ec5bd04f68cdfd19c4
Adam Pierzchała <pierzchala.adam@gmail.com>
2015-04-19 17:20:52 +0200

    messenger as component

--- src/main/java/hello/HelloMessenger.java
+++ src/main/java/hello/HelloMessenger.java
@@ -1,5 +1,8 @@
 package hello;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class HelloMessenger implements Messenger {
     @Override
     public String compose() {
--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -1,12 +1,18 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
 
-    Messenger messenger = new HelloMessenger();
+    Messenger messenger;
+
+    @Autowired
+    public SampleController(Messenger messenger) {
+        this.messenger = messenger;
+    }
 
     @RequestMapping("/")
     public String index() {
