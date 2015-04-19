bd61b869fb48e18e7fb5ad95b1be6f9bd75b7e8a
Adam Pierzchała <pierzchala.adam@gmail.com>
2015-04-19 17:08:19 +0200

    01-start

---
+++ src/main/java/hello/SampleController.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Sample Application!";
+    }
+
+}
+
